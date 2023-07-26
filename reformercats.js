fafc4e42e5efaa2269f7f0dcace0527b723eb1bd
jochui <joliec4@gmail.com>
2023-07-26 10:52:23 -0700

    reformer page

---
+++ reformercats.js
@@ -0,0 +1,14 @@
+function showone () { // click on the first image
+    var div = document.getElementById("div"); // get the div
+    div.innerHTML = "Great Expectations by Charles Dickens: The Reformer would enjoy this classic novel as it explores themes of self-improvement, personal growth, and a quest for positive change."; // change the text (inner html)
+  }
+
+  function showtwo () { // click on the first image
+    var div = document.getElementById("div"); // get the div
+    div.innerHTML = "Mary Poppins: The Reformer would appreciate this movie as it features a magical nanny who brings order and transformation to a family, aligning with their desire for positive change and practical advice."; // change the text (inner html)
+  }
+
+  function showthree () { // click on the first image
+    var div = document.getElementById("div"); // get the div
+    div.innerHTML = "Vincenzo: The Reformer would be drawn to this TV show as it centers on a character seeking justice and reform, highlighting themes of ethics and making a positive impact on society."; // change the text (inner html)
+  }
