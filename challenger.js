0bc5ab66cbd14d485e760fae781ddeff02cfa124
Karina <karikckarin@gmail.com>
2023-07-31 12:14:52 -0700

    hi

---
+++ challenger.js
@@ -0,0 +1,15 @@
+function showone () { // click on the first image
+    var div = document.getElementById("div"); // get the div
+    div.innerHTML = "Emma by Jane Austen: The Helper would enjoy this classic novel because it showcases a character dedicated to assisting and supporting those around her, resonating with their nurturing nature."; // change the text (inner html)
+  }
+
+  function showtwo () { // click on the first image
+    var div = document.getElementById("div"); // get the div
+    div.innerHTML = "The Princess Bride: The Helper would appreciate this movie for its themes of love, loyalty, and self-sacrifice, which align with their desire to be there for others."; // change the text (inner html)
+  }
+
+  function showthree () { // click on the first image
+    var div = document.getElementById("div"); // get the div
+    div.innerHTML = "Greys Anatomy: The Helper would be drawn to this TV show as it revolves around a group of compassionate and supportive doctors, reflecting their preference for shows with a focus on helping otherss."; // change the text (inner html)
+    
+  }
