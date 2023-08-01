e8038819774f7f76ced3c9bfd36b7954695f4c3f
jochui <joliec4@gmail.com>
2023-08-01 10:00:36 -0700

    achiever page

---
+++ achiever.js
@@ -0,0 +1,14 @@
+function showone () { // click on the first image
+    var div = document.getElementById("div"); // get the div
+    div.innerHTML = "The Atlas Six by Olivie Blake: The Achiever would be drawn to this book as it revolves around a group of highly ambitious and talented individuals who aim to achieve greatness and unlock hidden secrets, resonating with their desire for success."; // change the text (inner html)
+  }
+
+  function showtwo () { // click on the first image
+    var div = document.getElementById("div"); // get the div
+    div.innerHTML = "Aladdin: The Achiever would enjoy this movie as it features a determined protagonist who uses his ambition and resourcefulness to overcome obstacles and change his destiny."; // change the text (inner html)
+  }
+
+  function showthree () { // click on the first image
+    var div = document.getElementById("div"); // get the div
+    div.innerHTML = "The Great: The Achiever would appreciate this TV show as it centers on the rise of a young woman determined to become a powerful leader, showcasing ambition, wit, and a drive for advancement."; // change the text (inner html)
+  }
